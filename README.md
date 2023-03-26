ef6005f2487f9a6587f79bfa07bc3cbcb1c5246f
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-26 23:49:04 +0530

    3 files modified, 3 added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Context-Api-Project
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rgvxjc)
+
+* code
+Youtube - https://www.youtube.com/watch?v=gQ_l-1zpVBo&t=760s
+GitHub - https://github.com/piyush-eon/youtube-context-api-tutorial 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./style.css";
+import {BroserRouter as Router, Routes, Route} from 'react-router-dom';
 
-export default function App() {
+import Header from './components/Header';
+
+const App = ()=> {
+
   return (
+    <Router>
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+     
+      <h1>App.js</h1>
+      <Header />   
+      <Routes>
+        <Route path='/' element={  }  />
+      </Routes>
+
+    
     </div>
+    </Router>
   );
 }
+
+export default App;
